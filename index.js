bf05ad6131d2efadcbe7b54d4d0b87e375c40734
Avi Aryan <avi.aryan123@gmail.com>
2018-02-09 19:03:57 +0530

    basic chatting now works

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const io = require('socket.io-client');
+const readline = require('readline');
 
-const URL = 'https://chattt.glitch.me';
-// const URL = 'http://localhost:3000'; 
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+// const URL = 'https://chattt.glitch.me';
+const URL = 'http://localhost:3000'; 
 
 const socket = io(URL);
 
 socket.on('connect', () => {
 	console.log('connected');
+	socket.emit('/join', {channel: 'test', user: 'avi'});
+
+	socket.on('/msg test', function (msg) {
+		if (msg.user === null){
+			console.log(`[ ${msg.message} ]`);
+		} else {
+			console.log(`${msg.user}: ${msg.message}`);
+		}
+	});
+
+	let getInput = () => {
+		rl.question('', (inp) => {
+			// console.log('Got input ' + inp);
+			socket.emit('/msg test', { user: 'avi', message: inp });
+			// rl.close();
+			getInput();
+		});
+	};
+	getInput();
+
 });
