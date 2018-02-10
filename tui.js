2a83f576202ebd08b3f2db5669d6c0621c0b0c9c
Avi Aryan <avi.aryan123@gmail.com>
2018-02-10 19:22:57 +0530

    remove old comments

--- tui.js
+++ tui.js
@@ -10,47 +10,18 @@ let input = require('./ui/input');
 // const URL = 'https://chattt.glitch.me';
 const URL = 'http://localhost:3000';
 
+// server connection
 const socket = io(URL);
 let channel, user;
 
-
-// Append our box to the screen.
+// Append elements to the screen.
 screen.append(box.box);
 screen.append(input.input);
 box.screen = screen;
 
-// If our box is clicked, change the content.
-// box.on('click', function (data) {
-// 	box.setContent('{center}Some different {red-fg}content{/red-fg}.{/center}');
-// 	screen.render();
-// });
-
-// If box is focused, handle `enter`/`return` and give us some more content.
-// box.key('enter', function (ch, key) {
-// 	box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
-// 	box.setLine(1, 'bar');
-// 	box.insertLine(1, 'foo');
-// 	screen.render();
-// });
-
-// Focus our element.
-// box.focus();
-// input.read((val) => {
-// 	for (let i = 0; i < 100; i++) {
-// 		box.batchAdd('Joining channel ' + val + " " + i);
-// 		// box.setLine(i+1, ' Joining channel ' + value + " " + i);
-// 	}
-// 	box.finishAdding();
-// 	// box.setScrollPerc(100);
-// 	// box.focus();
-// 	// box.setContent(' Joining channel ' + value);
-// 	// box.focus();
-// 	// screen.render();
-// });
-
 // when socket connects
 socket.on('connect', () => {
-	box.addAnn('Connected to the server');
+	box.addAnn('Connected to the server ' + URL);
 
 	// join a channel
 	let userJoin = () => {
